6c9306fb8f6c58d0f759de219fb46b4ebd1bb340
Alessandro Moura <alessandromoura35@googlemail.com>
2026-04-05 21:37:01 +0100

    Fixed bug in dyn array.

--- src/dyn_arr_templ.c
+++ src/dyn_arr_templ.c
@@ -1,25 +1,22 @@
-TYPE_arr
-make_TYPE_arr_with_cap(arena *ar, size_t cap)
-{
+TYPE_arr make_TYPE_arr_with_cap(arena * ar, size_t cap) {
     assert(ar);
     TYPE_arr a = {0};
-    a.cap = cap;
-    a.v = arena_alloc_type_n(ar, TYPE, cap);
+    a.cap      = cap;
+    a.v        = arena_alloc_type_n(ar, TYPE, cap);
     return a;
 }
 
-void
-TYPE_arr_push(arena *ar, TYPE_arr *a, TYPE el)
-{
+void TYPE_arr_push(arena * ar, TYPE_arr * a, TYPE el) {
     assert(ar);
     assert(a);
     assert(a->len <= a->cap);
     if (a->len == a->cap) {
-        size_t new_cap = 1 + 2*a->cap;
-        new_cap = maximum(new_cap, 4);
-        a->v = arena_alloc_type_n(ar, TYPE, new_cap);
+        TYPE * prev_data = a->v;
+        size_t new_cap   = 1 + 2 * a->cap;
+        new_cap          = maximum(new_cap, 4);
+        a->v             = arena_alloc_type_n(ar, TYPE, new_cap);
+        memcpy(a->v, prev_data, a->len * sizeof(TYPE));
         a->cap = new_cap;
     }
     a->v[a->len++] = el;
 }
-
